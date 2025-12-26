b2c71bb847d36540be91b675a9b8dbb4ad78bf39
sohil-khann <khaa40408@gmail.com>
2025-12-26 12:41:32 +0530

    [Sohil khan] Add. Find Area of Circle

---
+++ Level_01/AreaOfCircle/Circle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+    internal class Circle
+    {
+        int radius;
+      public  Circle(int r)
+        {
+            this.radius = r;
+        }
+        public double CalculateArea()
+        {
+            return Math.PI * radius * radius;
+        }
+        public double CalculateCircumference()
+        {
+            return 2 * Math.PI * radius;
+        }
+        public void Display()
+        {
+            Console.WriteLine("Radius: " + radius);
+            Console.WriteLine("Area: " + CalculateArea());
+            Console.WriteLine("Circumference: " + CalculateCircumference());
+        }
+    }
+
